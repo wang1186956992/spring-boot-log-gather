a75fa94e8952ef3b8f3bbcf032e8626e49ba838a
xuejike <xuejike2004@126.com>
2017-08-14 16:08:07 +0800

    AOP修改代理模式

--- src/main/java/com/bidanet/log/LogAutoConfig.java
+++ src/main/java/com/bidanet/log/LogAutoConfig.java
@@ -3,12 +3,14 @@ package com.bidanet.log;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 
 /**
  * Created by xuejike on 2017/8/9.
  */
 @Configuration
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 @Import({LogAspect.class})
 public class LogAutoConfig {
 
